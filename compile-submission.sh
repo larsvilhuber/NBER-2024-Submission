8376327d4956624fa79f94ddb6bf373773743a64
Lars Vilhuber <lars.vilhuber@cornell.edu>
2025-04-10 16:53:07 -0400

    Adding the ZIP file and PDF as submitted

---
+++ compile-submission.sh
@@ -0,0 +1,1 @@
+zip -rp NBER_2024_Vilhuber-submitted-20250410.zip main.tex abstract.tex acrodefs.tex listings-stata.tex config.do hdsr.cls  *txt main.bbl references.bib table-coverage.tex responses.tex figs/ 
